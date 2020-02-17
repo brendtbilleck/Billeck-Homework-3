42a677fe0576ab2129667075ff7a43128ebabc59
Brendt Billeck <brendtbilleck.com>
2020-02-17 13:39:14 -0500

    Initial Commit

---
+++ Billeck Homework 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Billeck Homework 3
+//
+//  Created by Brendt Billeck on 2/17/20.
+//  Copyright © 2020 Brendt Billeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Billeck Homework 3Tests/Billeck_Homework_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Billeck_Homework_3Tests.swift
+//  Billeck Homework 3Tests
+//
+//  Created by Brendt Billeck on 2/17/20.
+//  Copyright © 2020 Brendt Billeck. All rights reserved.
+//
+
+import XCTest
+@testable import Billeck_Homework_3
+
+class Billeck_Homework_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
